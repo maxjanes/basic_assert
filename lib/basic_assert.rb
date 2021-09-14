3f1bf89824b6009696edd105388d2a0154b37d12
Massimiliano Janes <massimiliano.janes@uqido.com>
2021-09-14 09:16:52 +0200

    Hallo world

---
+++ lib/basic_assert.rb
@@ -0,0 +1,6 @@
+require "basic_assert/version"
+
+module BasicAssert
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/basic_assert/version.rb
@@ -0,0 +1,3 @@
+module BasicAssert
+  VERSION = "0.1.0"
+end
